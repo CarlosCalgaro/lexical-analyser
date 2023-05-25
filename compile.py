7fb4550a323bd484835bd1e0653320092a7b459d
Carlos Alberto Calgaro Filho <carloscalgarof@gmail.com>
2023-05-25 19:31:40 -0300

    Added options to command line argument

--- compile.py
+++ compile.py
@@ -1,8 +1,20 @@
 import sys
 from compiler import Compiler
+import argparse
 
 if __name__ == '__main__':
-   path = sys.argv[1]
-   compiler = Compiler()
-   with open(path, 'r') as file:
-      compiler.compile(file)
+   parser = argparse.ArgumentParser(description='Lexical Compiler C')
+   parser.add_argument('source_file', help='Path to the source file (e.g., ./main.c)')
+   parser.add_argument('--output', help='Path to the output file (e.g., output.txt)')
+   args = parser.parse_args()
+
+   input_stream = open(args.source_file, 'r')
+   if(args.output):
+      output_stream = open(args.output, 'w')
+   else:
+      output_stream = sys.stdout
+   compiler = Compiler(output_stream=output_stream)
+   compiler.compile(input_stream)
+
+   input_stream.close()
+   output_stream.close()
